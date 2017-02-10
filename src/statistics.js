b834b4c23a0fb14544282132e3ffeab2941c8806
Thomas Elder <thomas.charles.elder@gmail.com>
2017-02-10 19:46:28 +1100

    Stubbed out a new sub-module for Statistics functions
    Created a new module for statistics functions, and a new spec for this
    module. Added a couple of notes to the api.txt doc, which should
    probably be renamed like... project diary or something.

---
+++ src/statistics.js
@@ -0,0 +1,33 @@
+
+var size = function() {
+
+};
+
+var mean = function() {
+
+};
+
+var median = function() {
+
+};
+
+var mode = function() {
+
+};
+
+var variance = function(){
+
+};
+
+var standardDeviation = function() {
+
+};
+
+module.exports = {
+  size,
+  mean,
+  median,
+  mode,
+  variance,
+  standardDeviation
+}
